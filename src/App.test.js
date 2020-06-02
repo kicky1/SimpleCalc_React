189e2d8f31a40ab4b38340f791037efcbf011a7f
Krzysztof <krzysztofwicki1@gmail.com>
2020-06-02 22:40:20 +0200

    simpleCalc

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Calculator from './Calculator';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Calculator />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
